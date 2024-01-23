0ff13d70e3950117f39d453ce142abd3d3d1e8d8
Fernando Amthauer <fernando.amthauer@coolebra.com>
2024-01-23 10:00:00 -0300

    Change: Redirect url in login view

--- backend/usuario/views.py
+++ backend/usuario/views.py
@@ -164,7 +164,7 @@ class LoginUser(APIView):
         refresh['user_data'] = user_data
         refresh = jwt.encode(refresh, SECRET_KEY, algorithm="HS256")
     
-        return redirect("http://localhost:3000/login?token=" + str(refresh))
+        return redirect("http://localhost:3000/auth?token=" + str(refresh))
 
 
 def authenticate_or_create_user(data):
