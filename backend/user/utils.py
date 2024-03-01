6888904bc226fab56f5b192034619c17e8c7f3b0
VicenteOlmos <71791058+VicenteOlmos@users.noreply.github.com>
2024-03-01 11:10:30 -0300

    Update utils.py

--- backend/user/utils.py
+++ backend/user/utils.py
@@ -1,13 +1,16 @@
 def generate_avatar_url(email):
-    print("email: ",email,flush=True)
-    base_url = "https://ui-avatars.com/api/"
-    name, _ = email.split('@')[0].split('.')
-    formatted_name = name.replace('.', ' ').title()
-    params = "?"
-    params += "name=" + formatted_name.replace(" ", "+")
-    params += "&background=random"
-    params += "&color=fff"
-    params += "&length=2"
-    params += "&bold=true"
-    params += "&size=128"
-    return base_url + params
+    try:
+        print("email: ",email,flush=True)
+        base_url = "https://ui-avatars.com/api/"
+        name, _ = email.split('@')[0].split('.')
+        formatted_name = name.replace('.', ' ').title()
+        params = "?"
+        params += "name=" + formatted_name.replace(" ", "+")
+        params += "&background=random"
+        params += "&color=fff"
+        params += "&length=2"
+        params += "&bold=true"
+        params += "&size=128"
+        return base_url + params
+    except:
+        return None
