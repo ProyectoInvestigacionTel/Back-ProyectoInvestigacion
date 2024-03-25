4c967d261d10b309a34889fd64825486c9628efe
VicenteOlmos <71791058+VicenteOlmos@users.noreply.github.com>
2024-03-25 19:52:14 -0300

    Update add_picture.py

--- backend/user/management/commands/add_picture.py
+++ backend/user/management/commands/add_picture.py
@@ -1,13 +1,15 @@
 from django.core.management.base import BaseCommand
 from user.models import CustomUser
 from user.utils import generate_avatar_url
+from django.db.models import Q
 
 
 class Command(BaseCommand):
     help = "Actualiza los avatares de los usuarios sin imagen de perfil"
 
     def handle(self, *args, **kwargs):
-        users_without_picture = CustomUser.objects.filter(picture__isnull=True)
+        users_without_picture = CustomUser.objects.filter(Q(picture__isnull=True) | Q(picture__exact=""))
+
         for user in users_without_picture:
             user.picture = generate_avatar_url(user.name)
             user.save()
